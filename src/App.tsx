7010b624148a8584a40739c14052e7e62bfe7aab
Leticia Betman <leticiarodriguesbetman@gmail.com>
2025-09-12 19:25:03 -0300

    Etapa 2 - Rotas configuradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="bg-blue-600 p-4 text-white flex justify-between">
       <h1 className="text-xl font-bold">Farmácia</h1>
       <div className="space-x-4">
-        <a href="/" className="hover:underline">
-          Home
-        </a>
-        <a href="/categorias" className="hover:underline">
-          Categorias
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/categorias">Categorias</Link>
       </div>
     </nav>
   );
